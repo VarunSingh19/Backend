61f9474e4d5f6b95d770a08354a7bb36956af84f
VarunSingh19 <varunsingh18@icloud.com>
2024-08-13 21:40:07 +0530

    api response and error handling

---
+++ src/index.js
@@ -0,0 +1,61 @@
+// import mongoose from "mongoose";
+// import DB_NAME from constants;
+import connectDB from "./db/index.js";
+// require('dotenv').config({ path: './env' })
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is listening at PORT :${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection failed!!! ", err)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.log("Error :", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`server is listening at PORT : ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error :", error)
+        throw error
+    }
+})()
+import constants from "./constants";
+*/
